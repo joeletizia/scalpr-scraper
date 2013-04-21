92aeef52fd666c3e0b9eaca8f0cd9603a8d7ec72
Joe Letizia <joe.letizia@gmail.com>
2013-04-21 11:56:10 -0400

    changed classes to look for in order to get script working

--- craigslist.rb
+++ craigslist.rb
@@ -15,30 +15,34 @@ class Webscrape
     url = "http://#{city}.craigslist.org/#{cl_category}/"
     puts "Connecting..."
     agent = Mechanize.new
-      puts "Connected!"
+      puts "Mechanize Started!"
       searchpage = agent.get(url)
       index = 0
       results_array = []
       keep_going = true
       while keep_going == true or index < 1000000 #will never go over a million
+        puts "Current results: #{results_array.length}"
         index_term = (index != 0) ? "index" + index.to_s + ".html" : ""
         searchpage = agent.get(url+index_term)
-
-        links = searchpage.links.find_all { |l| l.attributes.parent.name == 'p' }
+        puts "searching #{url} term: #{index_term}"
+        safe_links = searchpage.links.reject{|l| l.attributes.parent.attributes['class'].nil?}
+        links = safe_links.find_all { |l| l.attributes.parent.attributes['class'].value == 'pl' }
         if links.size < 2
+          puts "last page!"
           keep_going = false
         end
+        puts "found #{links.length} on page"
         links.each do |l|
-          if l.attributes.parent["class"] == 'row'
-            new_page = l.click
-            table = new_page.parser.xpath('//section[@id="userbody"]')
-            text =  table.inner_text.to_s
-            number = /[\(\.\- ]*[0-9]{3}[\)\.\- ]*[0-9]{3}[\-\. ]*[0-9]{4}/.match(text)
-            if number
-              content = text.gsub(/\s+/," ")
-              puts "number: " + number.to_s
-              results_array.push [number, content]
-            end
+          new_page = l.click
+          puts "navigating to #{l.href}"
+          table = new_page.parser.xpath('//section[@class="userbody"]')
+          text =  table.inner_text.to_s
+          number = /[\(\.\- ]*[0-9]{3}[\)\.\- ]*[0-9]{3}[\-\. ]*[0-9]{4}/.match(text)
+          puts "searching for phone number"
+          if number
+            content = text.gsub(/\s+/," ")
+            puts "number: " + number.to_s
+            results_array.push [number, content]
           end
         end
         index += 100
