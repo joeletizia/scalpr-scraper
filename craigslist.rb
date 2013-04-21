e44713572d07c1d94432a4aff06ec5d5db159b63
Joe Letizia <joe.letizia@gmail.com>
2013-04-21 11:10:28 -0400

    initial commit

---
+++ craigslist.rb
@@ -0,0 +1,63 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'mechanize'
+require 'csv'
+require 'socksify'
+require 'socksify/http'
+require 'tor-privoxy'
+
+class Webscrape
+  def self.scrape_craigslist(city="sanfrancisco")
+    ##CONFIGURATION
+    cl_category = "tia"
+    ##END CONFIG
+    url = "http://#{city}.craigslist.org/#{cl_category}/"
+    puts "Connecting..."
+    agent = Mechanize.new
+      puts "Connected!"
+      searchpage = agent.get(url)
+      index = 0
+      results_array = []
+      keep_going = true
+      while keep_going == true or index < 1000000 #will never go over a million
+        index_term = (index != 0) ? "index" + index.to_s + ".html" : ""
+        searchpage = agent.get(url+index_term)
+
+        links = searchpage.links.find_all { |l| l.attributes.parent.name == 'p' }
+        if links.size < 2
+          keep_going = false
+        end
+        links.each do |l|
+          if l.attributes.parent["class"] == 'row'
+            new_page = l.click
+            table = new_page.parser.xpath('//section[@id="userbody"]')
+            text =  table.inner_text.to_s
+            number = /[\(\.\- ]*[0-9]{3}[\)\.\- ]*[0-9]{3}[\-\. ]*[0-9]{4}/.match(text)
+            if number
+              content = text.gsub(/\s+/," ")
+              puts "number: " + number.to_s
+              results_array.push [number, content]
+            end
+          end
+        end
+        index += 100
+      end
+      CSV.open("craigslist.csv", "a") do |csv|
+        results_array.each do |row|
+          csv << row
+        end
+      end
+  end
+end
+
+if __FILE__ == $0
+  if ARGV.size > 0
+    File.open( ARGV[0] ).each_line do |line| 
+      Webscrape.scrape_craigslist( line.strip )
+    end
+  else
+    Webscrape.scrape_craigslist
+  end
+end
+
